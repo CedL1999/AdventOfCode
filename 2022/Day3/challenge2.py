06d1fc43c694435e8b4b3f42d48bae6a318b4856
CedL1999 <cedlav@gmail.com>
2022-12-08 18:18:33 -0500

    challenge 2 of day 3 complete

---
+++ 2022/Day3/challenge2.py
@@ -0,0 +1,18 @@
+import string 
+with open('input/input.txt') as f:
+    formatted = [x.replace('\n','') for x in f.readlines() ]
+
+alphabet = list(string.ascii_lowercase) + list(string.ascii_uppercase)
+
+tuppleList = []
+#Create list a tupple with both halfs
+for x in range (0, len(formatted), 3):
+    tuppleList.append((formatted[x], formatted[x+1], formatted[x+2]))
+
+#Iterate through each tupple to see the common letter from its two elements
+sumOfCharacters = 0
+for x in tuppleList:
+    commonCharacter = ''.join(set(x[0]).intersection(x[1]).intersection(x[2]))
+    sumOfCharacters+=alphabet.index(commonCharacter) + 1 #adding 1 because the index count starts at 0
+
+print(sumOfCharacters)
